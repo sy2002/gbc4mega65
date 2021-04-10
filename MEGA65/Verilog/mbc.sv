4421dd1081ed017e68e328b7e9dac32d1781be31
sy2002 <code@sy2002.de>
2021-04-11 00:04:31 +0200

    WIP cartridge RAM: fixed cartridge RAM bank select bug
    All Mooneye RAM/ROM Tests of MBC1, MBC2 and MBC5 are passing now but the following ones in the MBC2 realm: bits_ramg.bg, bits_romg.gb, ram.gb

--- MEGA65/Verilog/mbc.sv
+++ MEGA65/Verilog/mbc.sv
@@ -163,7 +163,7 @@ always @(posedge clk_sys) begin
 		
 		//write to ROM bank register
 		if(cart_wr && (cart_addr[15:13] == 3'b001)) begin
-			if(~mbc5 && cart_di[6:0]==0) //special case mbc1-3 rombank 0=1
+			if(~mbc5 && (cart_di[6:0]==0 || (mbc1 && cart_di[4:0]==0) || (mbc2 && cart_di[3:0]==0))) //special case mbc1-3 rombank 0=1
 				mbc_rom_bank_reg <= 5'd1;
 			else if (mbc5) begin
 				if (cart_addr[13:12] == 2'b11) //3000-3FFF High bit
