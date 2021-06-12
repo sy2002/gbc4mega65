eaa9fd8c74b91921d99bab4cc6a6098798912649
sy2002 <code@sy2002.de>
2021-06-12 17:12:13 +0200

    minor cosmetics

--- MEGA65/vhdl/mega65.vhd
+++ MEGA65/vhdl/mega65.vhd
@@ -16,9 +16,9 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use work.qnice_tools.all;
 
 library work;
+use work.qnice_tools.all;
 use work.types_pkg.all;
 use work.video_modes_pkg.all;
 
