c940cb0c54d040518cdcd587100fee590698f0c0
sy2002 <code@sy2002.de>
2021-07-15 15:32:19 +0200

    Merged upstream HDMI fix from tyto_project
    This fix is meant to fix the following error from the HDMI analyzer: "CN1/CN0 1(ITC) 11(Game) Error M1-M0 = 0b10(16:9) of AVI InfoFrame Packet should correspond to the aspect ratio of the viewed image. Skip because AVI R3-R0 is not 1000(Same as picture aspect ratio) Test ID 7-27 : FAIL"
    
    https://github.com/amb5l/tyto_project/issues/5

--- MEGA65/vhdl/drivers/hdmi/vga_to_hdmi.vhd
+++ MEGA65/vhdl/drivers/hdmi/vga_to_hdmi.vhd
@@ -249,7 +249,7 @@ architecture synth of vga_to_hdmi is
     constant hb_3 : u8(0 to 2) := ( x"82", x"02", x"0D" );
     constant pb_3 : u8(0 to 27) := (
             0 => x"00",     -- *NOT CONSTANT* checksum
-            1 => x"12",     -- RSVD,Y(1:0),A0,B(1:0),S(1:0)
+            1 => x"02",     -- RSVD,Y(1:0),A0,B(1:0),S(1:0)
             2 => x"00",     -- *PART CONSTANT* C(1:0),M(1:0),R(3:0)
             3 => x"80",     -- ITC,EC(2:0),Q(1:0),SC(1:0)
             4 => x"00",     -- *NOT CONSTANT* VIC
@@ -363,7 +363,6 @@ begin
         end function xor_v;
 
     begin
-      
         if pcm_rst = '1' then
 
             iec_count   <= 0;
@@ -482,7 +481,6 @@ begin
         dest_pulse => vga_acr
         );
     
-
     SYNC : xpm_cdc_array_single
         generic map (
             DEST_SYNC_FF    => 2,
@@ -731,8 +729,6 @@ begin
                     pb(3)(5)(3 downto 0)
                 );
             pb(3)(2)(5 downto 4) <= unsigned(aspect_s);
-            pb(3)(2)(3) <= '1';
-            pb(3)(2)(1 downto 0) <= unsigned(aspect_s);
             pb(3)(4) <= unsigned(vic_s);
             pb(3)(5)(0) <= pix_rep_s;
             pb(3)(6 to 27) <= pb_3(6 to 27);
@@ -780,7 +776,7 @@ begin
                             if data_req /= "000000" then
                                 s1_period <= DATA_PRE; s1_enc <= ENC_DVI; s1_ctl <= CTL_PRE_DATA;
                                 s1_pcount <= (others => '0'); s1_dcount <= (others => '0');
-                                for i in 0 to (PACKET_TYPES-1) loop -- prioritize
+                                for i in 0 to PACKET_TYPES-1 loop -- prioritize
                                     p := i;
                                     exit when data_req(p) = '1';
                                 end loop;
@@ -832,7 +828,7 @@ begin
                         then -- do another data packet
                             s1_pcount <= (others => '0');
                             s1_dcount <= s1_dcount+1;
-                            for i in 0 to (PACKET_TYPES-1) loop -- prioritize
+                            for i in 0 to PACKET_TYPES-1 loop -- prioritize
                                 p := i;
                                 exit when data_req(p) = '1';
                             end loop;
