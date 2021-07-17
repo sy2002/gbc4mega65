5663c721653c255825165c3c97f1eb6da788472a
sy2002 <code@sy2002.de>
2021-07-17 19:21:09 +0200

    Plugged-in Adam Barnes' VGA timinig generator (hardcoded)
    TODO: If this solves the "CEA-861E Video Format Timing Error" as specified in https://github.com/amb5l/tyto_project/issues/2, then we need to remove the hard coded timings and re-test on several picky/pesky monitors, if everything (including sound) is still OK.

--- MEGA65/vhdl/video/vga_controller.vhd
+++ MEGA65/vhdl/video/vga_controller.vhd
@@ -11,6 +11,7 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity vga_controller is
    port (
@@ -38,6 +39,14 @@ end vga_controller;
 
 architecture synthesis of vga_controller is
 
+constant DEFAULT_COL : integer := 0;
+constant DEFAULT_ROW : integer := 0;
+
+signal ax            : std_logic_vector(11 downto 0);
+signal ay            : std_logic_vector(11 downto 0);
+signal vblank        : std_logic;
+signal hblank        : std_logic;
+
 begin
 
    -- TODO: SINCE THIS IS FOR NOW JUST AN EXPERIMENT, ALL TIMING PARAMETERS ARE HARDCODED
@@ -49,27 +58,50 @@ begin
 
          -- Input: Timing parameters for the video mode at hand
          -- TODO: SEE ABOVE / CHANGE HARDCODED TIMING
+         --            name        => "1280x720p60     ",
+         --            dmt         => false,
+         --            id          => 4,
+         --            clk_sel     => CLK_SEL_74M25,
+         --            pix_rep     => 0,
+         --            aspect      => ASPECT_16_9,
+         --            interlace   => FALSE,
+         --            v_tot       => 750,
+         --            v_act       => 720,
+         --            v_sync      => 5,
+         --            v_bp        => 20,
+         --            h_tot       => 1650,
+         --            h_act       => 1280,
+         --            h_sync      => 40,
+         --            h_bp        => 220,
+         --            vs_pol      => '1',
+         --            hs_pol      => '1'         
          pix_rep   => '0',                 -- pixel repetition; 0 = none/x1, 1 = x2
-         interlace => '0',: in    std_logic;
-         v_tot     => -- 1..2048 (must be odd if interlaced)
-         v_act     => -- 1..2048 (should be even)
-         v_sync    => -- 1..7
-         v_bp      => -- 1`..31
-         h_tot     => -- 1..4096
-         h_act     => -- 1..2048 (must be even)
-         h_sync    => -- 1..127
-         h_bp      => -- 0..255
-         align     => -- alignment delay
+         interlace => '0',
+         v_tot     => std_logic_vector(to_unsigned(750, 11)),
+         v_act     => std_logic_vector(to_unsigned(720, 11)),
+         v_sync    => std_logic_vector(to_unsigned(5, 3)),
+         v_bp      => std_logic_vector(to_unsigned(20, 6)),
+         h_tot     => std_logic_vector(to_unsigned(1650, 12)),
+         h_act     => std_logic_vector(to_unsigned(1280, 11)),
+         h_sync    => std_logic_vector(to_unsigned(40, 7)),
+         h_bp      => std_logic_vector(to_unsigned(220, 8)),
+         align     => (others => '0'),                  
          
          -- Output: Sync and blanking signals, field ID, beam position
-         f         => -- field ID
-         vs        => -- vertical sync
-         hs        => -- horizontal sync
-         vblank    => -- vertical blank
-         hblank    => -- horizontal blank
-         ax        => -- visible area X (signed)
-         ay        => -- visible area Y (signed)      
+         f         => open,      -- field ID
+         vs        => v_sync,    -- vertical sync
+         hs        => h_sync,    -- horizontal sync
+         vblank    => vblank,    -- vertical blank
+         hblank    => hblank,    -- horizontal blank
+         ax        => ax,        -- visible area X (signed)
+         ay        => ay         -- visible area Y (signed)      
       );
-
+      
+      
+   disp_ena  <= not (vblank or hblank);
+   column    <= to_integer(signed(ax)) when disp_ena = '1' else DEFAULT_COL;
+   row       <= to_integer(signed(ay)) when disp_ena = '1' else DEFAULT_ROW;
+   n_blank   <= not disp_ena;
+   n_sync    <= not (h_sync or v_sync);      
 
 end architecture synthesis;
